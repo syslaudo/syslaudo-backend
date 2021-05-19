86666390b6d2786f4814d9689622b2662e8c719a
Natan Nascimento <u006499@xpmac-10037235.xpcorretora.com.br>
2021-05-19 13:42:37 -0300

    Updating server with environment variables config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import './database/connect';
 
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 
@@ -8,10 +9,12 @@ import routes from './routes';
 
 const app = express();
 
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () =>
+app.listen(process.env.APP_PORT || 3000, () =>
   console.log('🔥 Server started at http://localhost:3000'),
 );
