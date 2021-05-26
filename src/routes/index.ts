6bd2e6ea77969c8913f98d2c8c0ef4d9b603e7e2
AlissonSantos17 <alisson.wiin@gmail.com>
2021-05-26 15:47:59 -0300

    delete: Removendo codigo dos medicos pois esta sendo usado junto a usuario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import usuarioRoutes from './usuario.routes';
 import sessionRoutes from './session.routes';
 import pacienteRoutes from './paciente.routes';
-// import medicoRouter from './medico.routes';
 
 const router = Router();
 
@@ -12,9 +11,6 @@ router.use('/usuario', usuarioRoutes);
 
 // ROTA PARA INICIAR SESSÃO;
 router.use('/session', sessionRoutes);
-
-// ROTA PARA CRIAR, LISTAR E APAGAR MÉDICO;
-// router.use('/medico', medicoRoutes);
 
 // ROTA PARA CRIAR, LISTAR E APAGAR PACIENTE;
 router.use('/paciente', pacienteRoutes);
