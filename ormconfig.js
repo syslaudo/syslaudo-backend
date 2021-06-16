20e5a9615839bf6933aae8da40ef8f3fc78468c0
Natan Nascimento <natanascimentom@outlook.com>
2021-06-16 19:01:09 -0300

    Update and rename ormconfig.json to ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+if (process.env.ENVIRONMENT == 'DEV'){
+  module.exports = {  
+  "type": process.env.DB_TYPE,
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_DATABASE,
+  "synchronize": false,
+  "logging": true,
+  "migrations": ["./src/database/migrations/**.ts"],
+  "entities": ["./src/models/**.ts"],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+  };
+} else if (process.env.ENVIRONMENT == 'PROD') {
+  module.exports = {  
+    "type": process.env.DB_TYPE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "synchronize": false,
+    "logging": false,
+    "migrations": ["dist/database/migrations/**.js"],
+    "entities": ["dist/models/**.js"],
+    "cli": {
+      "migrationsDir": "./src/database/migrations"
+    }
+  };
+};
